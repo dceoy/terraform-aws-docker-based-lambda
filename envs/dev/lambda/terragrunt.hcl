9c8d4326ae00ff6e83ba236e9678c58fa6a15e50
dceoy <dnarsil+github@gmail.com>
2024-06-12 00:57:06 +0900

    Add output variables

--- envs/dev/lambda/terragrunt.hcl
+++ envs/dev/lambda/terragrunt.hcl
@@ -21,13 +21,13 @@ dependency "s3" {
 dependency "docker" {
   config_path = "../docker"
   mock_outputs = {
-    docker_registry_image_uri = "123456789012.dkr.ecr.us-east-1.amazonaws.com/my-function:latest"
+    docker_registry_primary_image_uri = "123456789012.dkr.ecr.us-east-1.amazonaws.com/my-function:latest"
   }
   mock_outputs_merge_strategy_with_state = "shallow"
 }
 
 inputs = {
-  lambda_image_uri  = dependency.docker.outputs.docker_registry_image_uri
+  lambda_image_uri  = dependency.docker.outputs.docker_registry_primary_image_uri
   kms_key_arn       = dependency.kms.outputs.kms_key_arn
   s3_iam_policy_arn = dependency.s3.outputs.s3_iam_policy_arn
 }
--- modules/docker/outputs.tf
+++ modules/docker/outputs.tf
@@ -1,9 +1,19 @@
-output "docker_registry_image_uri" {
-  description = "Docker registry image URI"
+output "docker_registry_primary_image_uri" {
+  description = "Docker registry primary image URI"
   value       = docker_registry_image.primary.name
 }
 
-output "docker_registry_image_id" {
-  description = "Docker registry image ID"
+output "docker_registry_primary_image_id" {
+  description = "Docker registry primary image ID"
   value       = docker_registry_image.primary.id
+}
+
+output "docker_registry_secondary_image_uris" {
+  description = "Docker registry secondary image URIs"
+  value       = values(docker_registry_image.secondary)[*].name
+}
+
+output "docker_registry_secondary_image_ids" {
+  description = "Docker registry secondary image IDs"
+  value       = values(docker_registry_image.secondary)[*].id
 }
